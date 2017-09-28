72354299f36af156f53e4998a0f27486feb920f2
Lesley <lesley919@gmail.com>
2017-09-28 13:26:39 -0700

    adding js

--- main.js
+++ main.js
@@ -1,13 +1,77 @@
-function initMap() {
-    //(<map element>, <configuration object>)
-    var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.8054491, lng: -73.9654415},
-    zoom: 10,
+
+//drop-down menu
+$('#mp_menu').on('click', function(e){
+  e.preventDefault();
+  $('#sub_menu').slideToggle(200);
+
+});
+
+//button hover style
+
+$('#submit').on('mouseenter', function(){
+  $(this).addClass('btn_hover');
+});
+
+$('#submit').on('mouseleave', function(){
+  $(this).removeClass('btn_hover');
+});
+
+//inline validation function
+$('#name').on('blur', function(){
+  if ($('#name').val() === '') {
+    $('.name').text('*Please include your name');
+  } else {
+    $('.name').text('');
+  }
+});
+
+
+//pop-up date picker
+ $( function() {
+    $( "#date" ).datepicker();
   });
-   // var marker = new google.maps.Marker({
-   //   position: userLocation,
-   //   map: map,
-   //   title: 'User Location',
-    //});
+
+//$('#date').on('blur', function(){
+// if ($('#date').val() === '') {
+//    $('.date').html('*Please include your desired date');
+//  } else {
+//    $('.date').text('');
+//  }
+//});
+
+
+
+//submit form button
+$('#submit').on('click', function(e){
+  e.preventDefault();
+  //code for form submission here
+
+  if ($('#name').val() === '') {
+    $('.name').text('*Please include your name');
+  } else {
+    $('.name').text('');
+  };
+
+ if ($('#date').val() === '') {
+    $('.date').html('*Please include your desired date');
+  } else {
+    $('.date').text('');
+  };
+
+});
+
+//back-to-top
+
+function initMap() {
+//(<map element>, <configuration object>)
+  var map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 40.8054491, lng: -73.9654415},
+  zoom: 15,
+});
+ var marker = new google.maps.Marker({
+   position: {lat: 40.8054491, lng: -73.9654415},
+   map: map,
+   title: 'Monks Cafe',
+});
 }
 initMap();
